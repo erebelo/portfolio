a67ec2b2e0b9205e0d696a4ff2a5d9a7922cb470
erebelo <eduldrebelo@gmail.com>
2024-05-13 21:59:33 -0300

    Removing comment from index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
-import App from './App'
 import { ThemeProvider } from './contexts/theme'
+import App from './App'
 import './index.css'
 
 const container = document.getElementById('root')
-const root = createRoot(container) // createRoot(container!) if you use TypeScript
+const root = createRoot(container)
 root.render(
   <ThemeProvider>
     <App />
